ff85b02534a79bf2f2cedb714001bfb0fb241d77
Edward Munoz <alexmu419@icloud.com>
2023-06-29 01:06:56 -0500

    added outputs for events

--- script.js
+++ script.js
@@ -9,5 +9,21 @@ var wind = document.getElementById("wind");
 
 search.addEventListener("click", function() 
 {
-    fetch("https://api.github.com/users/" + document.getElementById("username").value)
+    fetch("http://api.weatherapi.com/v1/current.json?key=81c43d6896cc44ddaa052218232906&q=75054&aqi=yes")
+    .then(response => response.json())
+
+    .then(data => {
+        var cityValue = cityBox.value;
+        var nameValue = data.name;
+        var tempValue = data.main.temp;
+        var weatherValue = data.weather[0].description;
+        var humidityValue = data.main.humidity;
+        var windValue = data.wind.speed;
+        var iconValue = data.weather[0].icon;
+        var iconUrl = "http://openweathermap.org/img/w/" + iconValue + ".png";
+        var icon = document.getElementById("icon");
+        var iconImg = document.createElement("img");
+})
+
+.catch(err => alert("Wrong city name!"))
 });
